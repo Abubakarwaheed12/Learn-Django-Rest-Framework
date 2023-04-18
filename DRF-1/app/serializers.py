05ae63d4f167cf0d22ae6eff4a2aa0607f28b9e9
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-04-18 17:04:08 +0500

    ...

--- DRF-1/app/serializers.py
+++ DRF-1/app/serializers.py
@@ -6,6 +6,6 @@ from rest_framework import serializers
 class MyModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=MyModel
-        fields=['item_name']
+        fields=['uid' ,'item_name' , 'created_at']
         
         
