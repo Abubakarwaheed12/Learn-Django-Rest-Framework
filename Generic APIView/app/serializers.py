c97cd0a01e1079804bc384eec17267481ea2246e
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-05-05 14:53:03 +0500

    ..

--- Generic APIView/app/serializers.py
+++ Generic APIView/app/serializers.py
@@ -5,10 +5,10 @@ class StudentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Students
-        fields = ['name' , 'age' ,'father_name' ,'class_name','grade']
+        fields = ['id' ,'name' , 'age' ,'father_name' ,'class_name','grade']
         
     # Same Validation 
-    def validate_name(self, value):
-        if value[0].islower():
-            raise serializers.ValidationError('name always start with Capital letter ') 
+    # def validate_name(self, value):
+    #     if value[0].islower():
+    #         raise serializers.ValidationError('name always start with Capital letter ') 
             
--- Generic APIView/app/views.py
+++ Generic APIView/app/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
 from app.models import Students
-from app.serializers import StudentSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin , CreateModelMixin , RetrieveModelMixin , UpdateModelMixin , DestroyModelMixin
+from app.serializers import StudentSerializer  
+
 # Create your views here.
 
-# Generic view 
-
-
-@api_view(['GET','POST'])
-def ApiViewMethod(request):
-    if request.method =='GET':
-        return Response({'msg':'Get method is called'})
+# Class
+# get all students and create students class  
+class LCStudentApi(GenericAPIView , CreateModelMixin , ListModelMixin):
+    queryset=Students.objects.all()
+    serializer_class=StudentSerializer
     
-    if request.method =='POST':
-        return Response({'msg':'Post method is called' ,'data':request.data})
+    def get(self , request , *args , **kwargs ):
+        
+        return self.list(request , *args , **kwargs)
+    
+    def post(self ,request , *args , **kwargs):
+        
+        return self.create(request , *args , **kwargs)
+    
+    
+class RUDStudentApi(GenericAPIView , UpdateModelMixin , DestroyModelMixin , RetrieveModelMixin):
+    queryset=Students.objects.all()
+    serializer_class=StudentSerializer 
+    
+    def get(self ,request , *args , **kwargs):
+
+        return self.retrieve(request , *args , **kwargs)
+    
+    def put(self , request , *args , **kwargs):
+        
+        return self.update(request , *args , **kwargs)
+    
+    def delete(self , request , *args , **kwargs):
+        
+        return self.destroy(request , *args , **kwargs)
