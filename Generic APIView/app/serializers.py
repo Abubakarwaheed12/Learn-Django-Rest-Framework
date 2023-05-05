219df99b5e6bcdc22391cf585d8b986a29ea6445
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-05-05 10:40:15 +0500

    ..

---
+++ Generic APIView/app/serializers.py
@@ -0,0 +1,14 @@
+from app.models import Students 
+from rest_framework import serializers
+
+class StudentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Students
+        fields = ['name' , 'age' ,'father_name' ,'class_name','grade']
+        
+    # Same Validation 
+    def validate_name(self, value):
+        if value[0].islower():
+            raise serializers.ValidationError('name always start with Capital letter ') 
+            
---
+++ Generic APIView/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from app.models import Students
+from app.serializers import StudentSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# Create your views here.
+
+# Api View
+
+
+@api_view(['GET','POST'])
+def ApiViewMethod(request):
+    if request.method =='GET':
+        return Response({'msg':'Get method is called'})
+    
+    if request.method =='POST':
+        return Response({'msg':'Post method is called' ,'data':request.data})
