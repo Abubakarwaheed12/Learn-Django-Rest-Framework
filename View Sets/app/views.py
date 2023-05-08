59c9a6597dd69d985be37c44c4d7059bd65ff6ac
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-05-08 17:40:41 +0500

    ..

--- View Sets/app/views.py
+++ View Sets/app/views.py
@@ -9,39 +9,54 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 
-class StudentApi(viewsets.ViewSet):
-    """
-    A simple ViewSet for listing or retrieving users.
-    """
-    def list(self, request):
-        queryset = Students.objects.all()
-        serializer = StudentSerializer(queryset, many=True)
-        return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = Students.objects.get(id=pk)
-        serializer = StudentSerializer(queryset)
-        return Response(serializer.data)
+# ModelViewSet
+
+# class StudentApi(viewsets.ModelViewSet):
+#     queryset=Students.objects.all()
+#     serializer_class = StudentSerializer
+
+
+# ReadOnly ModelViewSet
+
+class StudentApi(viewsets.ReadOnlyModelViewSet):
+    queryset=Students.objects.all()
+    serializer_class = StudentSerializer
+
+
+# class StudentApi(viewsets.ViewSet):
+#     """
+#     A simple ViewSet for listing or retrieving users.
+#     """
+#     def list(self, request):
+#         queryset = Students.objects.all()
+#         serializer = StudentSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+#     def retrieve(self, request, pk=None):
+#         queryset = Students.objects.get(id=pk)
+#         serializer = StudentSerializer(queryset)
+#         return Response(serializer.data)
     
-    def create(self, request):
-        serializer=StudentSerializer(data=request.data)
+#     def create(self, request):
+#         serializer=StudentSerializer(data=request.data)
         
-        if serializer.is_valid():
-            serializer.save()
+#         if serializer.is_valid():
+#             serializer.save()
             
-            return Response({'msg':'saved successfully'})
+#             return Response({'msg':'saved successfully'})
         
-        return Response(serializer.errors)
+#         return Response(serializer.errors)
 
   
 
-    def update(self, request, pk=None):
-        pass
+#     def update(self, request, pk=None):
+#         pass
     
 
-    def partial_update(self, request, pk=None):
-        pass
+#     def partial_update(self, request, pk=None):
+#         pass
 
 
-    def destroy(self, request, pk=None):
-        pass
+#     def destroy(self, request, pk=None):
+#         pass
