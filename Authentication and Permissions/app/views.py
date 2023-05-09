51d414e039abb62041d9f5638c0f45b738e70248
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-05-09 10:48:31 +0500

    .

--- Authentication and Permissions/app/views.py
+++ Authentication and Permissions/app/views.py
@@ -7,55 +7,16 @@ from rest_framework import status
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Permission Classes
 
-# class StudentApi(viewsets.ModelViewSet):
-#     queryset=Students.objects.all()
-#     serializer_class = StudentSerializer
-
-
-# ReadOnly ModelViewSet
-
-class StudentApi(viewsets.ReadOnlyModelViewSet):
+class StudentApi(viewsets.ModelViewSet):
     queryset=Students.objects.all()
     serializer_class = StudentSerializer
-
-
-# class StudentApi(viewsets.ViewSet):
-#     """
-#     A simple ViewSet for listing or retrieving users.
-#     """
-#     def list(self, request):
-#         queryset = Students.objects.all()
-#         serializer = StudentSerializer(queryset, many=True)
-#         return Response(serializer.data)
-
-#     def retrieve(self, request, pk=None):
-#         queryset = Students.objects.get(id=pk)
-#         serializer = StudentSerializer(queryset)
-#         return Response(serializer.data)
     
-#     def create(self, request):
-#         serializer=StudentSerializer(data=request.data)
-        
-#         if serializer.is_valid():
-#             serializer.save()
-            
-#             return Response({'msg':'saved successfully'})
-        
-#         return Response(serializer.errors)
-
-  
-
-#     def update(self, request, pk=None):
-#         pass
-    
-
-#     def partial_update(self, request, pk=None):
-#         pass
+    authentication_classes=[BasicAuthentication]
+    permission_classes=[IsAuthenticated]
 
 
-#     def destroy(self, request, pk=None):
-#         pass
