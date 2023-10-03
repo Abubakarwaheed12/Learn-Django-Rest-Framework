43ee86b196af77aa1564098043fdf826263265f4
abubakarwaheed12 <abubakarjutt6346527@gmail.com>
2023-10-04 03:17:07 +0500

    new

--- JWT/app/models.py
+++ JWT/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    name=models.CharField(max_length=200)
+    age=models.IntegerField()
+    father_name = models.CharField(max_length=200)
+    class_name = models.CharField(max_length=20)
+    grade =models.CharField(max_length=20)
+    
+    
+    def  __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural='Our Students'
--- JWT/app/views.py
+++ JWT/app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import viewsets 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle , ScopedRateThrottle
+from rest_framework.filters import SearchFilter , OrderingFilter
+from rest_framework_simplejwt.authentication import JWTAuthentication 
+from .models import Students 
+from .serializers  import StudentSerializer
 # Create your views here.
+
+class StudentApi(viewsets.ModelViewSet):
+    queryset=Students.objects.all()
+    serializer_class = StudentSerializer
+    # permission_classes = [IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
+
+    # Throttling 
+    # throttle_classes = [UserRateThrottle]
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = 'viewstu'
+
+    # Search 
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
+    # multiple
+    # search_fields = ['name','age']
+    # start with 
+    search_fields = ['^name']
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset() 
+        parameter_value = self.request.query_params.get('parameter_name')
+        if parameter_value:
+            queryset = queryset.filter(your_field=parameter_value)
+        self.queryset = Students.objects.filter()
